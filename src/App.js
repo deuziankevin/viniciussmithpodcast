d559add61219e8b6808fb97bd04d99ecb4a84e43
Kevin Calheiros <deuziankevin@Outlook.com>
2020-07-08 16:30:35 -0400

    Front-end pronto 1.0

--- src/App.js
+++ src/App.js
@@ -1,25 +1,50 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import './global/global.css'
+
+import logoImg from './logo256.png'
+import platImg from './plat1.jpeg';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
+    <div className="logon-container">
+    <section className="form">
+    <img src={logoImg} alt="Logo"/>
+        <form>
+        <h3 color="white"> Apresentado por: André Vinicius </h3>
+            <h1 color="white"> Ouça agora </h1>
+
+            <a
+          className="button-spotify"
+          href="https://open.spotify.com/show/1LrOXnIWjlW6NikfnwmTdu"
           target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+          >
+            Spotify
+          </a>
+
+          <b
+          className="button-anchor"
+          href="https://hltv.org/"
+          target="_blank"
+          >
+            Anchor
+          </b>
+
+          <c
+          className="button-pocket"
+          href="https://open.spotify.com/show/1LrOXnIWjlW6NikfnwmTdu"
+          target="_blank"
+          >
+            Pocket
+          </c>
+
+            <h2 color="white"> Desenvolvido por Kevin Calheiros, 2020. </h2>
+
+        </form>
+    </section>
+    <img src={platImg} alt="Vinicius Podcast"/>
+</div>
   );
 }
 
